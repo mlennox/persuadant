eb139c063ad39331acc0893d81626cb1baa64bf6
Mark Lennox <mark@webpusher.ie>
2018-10-20 00:23:47 +0100

    swapped the dev server, added less watcher, stubbed out html

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # persuadant
 
 Experiments with responsive layouts and media queries
+
+## Requirements
+
+Uses node 8.9.3 to run the web server that will be accessible at `localhost:8080` after running
+
+```bash
+npm start
+```
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-var connect = require("connect");
-var serveStatic = require("serve-static");
-connect()
-  .use(serveStatic(__dirname))
-  .listen(8080, function() {
-    console.log("Server running on 8080...");
-  });
+var liveServer = require("live-server");
+
+var params = {
+  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+  ignore: "less",
+  wait: 1
+  // root: "/public", // Set root directory that's being served. Defaults to cwd.
+  // open: false, // When false, it won't load your browser by default.
+  // ignore: "scss,my/templates", // comma-separated string for paths to ignore
+  // file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+  // wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+  // mount: [["/components", "./node_modules"]], // Mount a directory to a route.
+  // logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+  // middleware: [
+  //   function(req, res, next) {
+  //     next();
+  //   }
+  // ] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+liveServer.start(params);
