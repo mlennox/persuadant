013bdc2faf4bdb7b1726333bebb6fef743ed2f73
Mark Lennox <mark@webpusher.ie>
2018-10-22 23:39:43 +0100

    trying to get server to ignore git files

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var liveServer = require("live-server");
 var params = {
   logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
   ignore: "less",
-  wait: 1
+  wait: 1,
+  ignore: "less,.git/**/*.*" // comma-separated string for paths to ignore
   // root: "/public", // Set root directory that's being served. Defaults to cwd.
   // open: false, // When false, it won't load your browser by default.
   // ignore: "scss,my/templates", // comma-separated string for paths to ignore
